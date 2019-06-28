2021a139ed75987389a4f56791db38a78729f174
JSHEN9085 <jshen9085@gmail.com>
2019-06-27 22:10:50 -0400

    rest controller get and post

--- src/main/java/com/jshen/booking/BookingController.java
+++ src/main/java/com/jshen/booking/BookingController.java
@@ -1,14 +1,13 @@
 package com.jshen.booking;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/bookings")
+@RequestMapping("/api/v1")
 public class BookingController {
     private List<HotelBooking> bookings;
 
@@ -19,8 +18,20 @@ public class BookingController {
         bookings.add(new HotelBooking("Marriot", 140.99, 1));
     }
 
-    @GetMapping
+    @GetMapping("bookings")
     public List<HotelBooking> getAll(){
+        return bookings;
+    }
+
+    @GetMapping("/bookings/affordable/{budget}") //variable "{budget}" must has the same name as the argument below
+    public List<HotelBooking> getAffordable(@PathVariable double budget){
+        return bookings.stream().filter(x -> x.getPricePerNight() <= budget).collect(Collectors.toList());
+    }
+
+    @PostMapping("/bookings")
+    public List<HotelBooking> createBooking(@RequestBody HotelBooking hotelBooking){
+        bookings.add(hotelBooking);
+
         return bookings;
     }
 
